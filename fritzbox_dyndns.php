240f2514f8ee15f6403432b84b83dbbcbf24f03a
Heathcliff <heathcliff@heathcliff.eu>
2023-05-02 23:07:44 +0200

    Return proper http response and set ttl to auto
    Allow the script to return a proper http response instead of just dying on
    error.
    Set the TTL of the records to auto, to allow the changes to propagate faster.
    Otherwise it might take clients up to 2h to pick up ip changes.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- fritzbox_dyndns.php
+++ fritzbox_dyndns.php
@@ -1,9 +1,9 @@
 <?php
 
 // Author: 1rfsNet
-// Version: 2.0.3
+// Version: 2.0.4
 // Created: 26.04.2020
-// Updated: 21.12.2022
+// Updated: 02.05.2023
 // License: GNU General Public License v3.0
 //
 // Description:
@@ -11,7 +11,7 @@
 //
 // Parameters:
 //   - Cloudflare api key (as password) - required
-//   - domain(s) (as domain) -> use multiple domains by separating by semicolon (;) - required
+//   - domain(s) (as domain) -> use multiple domains by separating by semicolon (,) - required
 //   - ipv4 (is automatically provided by the Fritz!Box) - optional (can be removed if you don't want to use ipv4)
 //   - ipv6 (is automatically provided by the Fritz!Box) - optional (can be removed if you don't want to use ipv6)
 //   - log (true/false) - optional (default: false)
@@ -19,10 +19,11 @@
 //   Fritz!Box update URL: https://example.com/fritz_dyndns.php?cf_key=<pass>&domain=<domain>&ipv4=<ipaddr>&ipv6=<ip6addr>&log=<true/false>&proxy=<true/false>
 
 wlog("INFO", "===== Starting Script =====");
+header("Content-Type: txt/plain");
 if ((!isset($_GET["cf_key"]) && empty($_GET["cf_key"])) || (!isset($_GET["domain"]) && empty($_GET["domain"]))) {
     wlog("ERROR", "Parameter(s) missing or invalid");
     wlog("INFO", "Script aborted");
-    die;
+    exit_error(400, "Parameter(s) missing or invalid");
 }
 if (isset($_GET["ipv4"]) && !empty($_GET["ipv4"]) && filter_var($_GET["ipv4"], FILTER_VALIDATE_IP, FILTER_FLAG_IPV4) == true) {
     wlog("INFO", "New IPv4: " . $_GET["ipv4"]);
@@ -42,7 +43,7 @@ if (isset($_GET["ipv6"]) && !empty($_GET["ipv6"]) && filter_var($_GET["ipv6"], F
 if (!$ipv4 && !$ipv6) {
     wlog("ERROR", "Neither IPv4 nor IPv6 available. Probably the parameters are missing in the update URL. Please note that the update URL has changed with the script version 2.0. You have to change this setting in your Fritz!Box.");
     wlog("INFO", "Script aborted");
-    die;
+    exit_error(400, "Neither IPv4 nor IPv6 available. Probably the parameters are missing in the update URL.");
 }
 
 if (isset($_GET["proxy"]) && $_GET["proxy"]) {
@@ -57,12 +58,14 @@ $auth = cf_curl("zones");
 if (!$auth["success"]) {
     wlog("ERROR", "Cloudflare authentication failed: " . $auth["errors"][0]["message"]);
     wlog("INFO", "Script aborted");
-    die;
-} else
+    exit_error(401, "Cloudflare authentication failed: " . $auth["errors"][0]["message"]);
+} else {
     wlog("INFO", "Cloudflare authentication successful");
+}
 
 wlog("INFO", "Found records to set: " . $_GET["domain"]);
 $domains = explode(",", $_GET["domain"]);
+$result = "success";
 
 foreach ($domains as $domain) {
     wlog("INFO", "Find zone for record '" . $domain . "'");
@@ -72,6 +75,7 @@ foreach ($domains as $domain) {
     $response = cf_curl("zones?name=" . $zone . "&status=active");
     if (!$response["success"]) {
         wlog("ERROR", "Could not set record '" . $domain . "', because the script could not determine the zone id (check api permissions and resources). Continue with next domain, if available.");
+        $result = "failure";
         continue;
     } else
         wlog("INFO", "Found zone id (" . $response["result"][0]["id"] . ") for '" . $domain . "'.");
@@ -81,17 +85,19 @@ foreach ($domains as $domain) {
     if (!$response) {
         wlog("INFO", "Could not find record, creating new record.");
         if ($ipv4) {
-            $response = cf_curl("zones/" . $zone . "/dns_records", array("type" => "A", "name" => $domain, "content" => $ipv4, "ttl" => 120, "proxied" => $proxy));
-            if (!$response["success"])
+            $response = cf_curl("zones/" . $zone . "/dns_records", array("type" => "A", "name" => $domain, "content" => $ipv4, "ttl" => 1, "proxied" => $proxy));
+            if (!$response["success"]) {
                 wlog("ERROR", "Could not create record for '" . $domain . "'. Continue with next record, if available.");
-            else
+                $result = "failure";
+            } else
                 wlog("INFO", "Created new A-Record for '" . $domain . "' with ip '" . $ipv4 . "' successfully.");
         }
         if ($ipv6) {
-            $response = cf_curl("zones/" . $zone . "/dns_records", array("type" => "AAAA", "name" => $domain, "content" => $ipv6, "ttl" => 120, "proxied" => $proxy));
-            if (!$response["success"])
+            $response = cf_curl("zones/" . $zone . "/dns_records", array("type" => "AAAA", "name" => $domain, "content" => $ipv6, "ttl" => 1, "proxied" => $proxy));
+            if (!$response["success"]) {
                 wlog("ERROR", "Could not create record for '" . $domain . "'. Continue with next record, if available.");
-            else
+                $result = "failure";
+            } else
                 wlog("INFO", "Created new AAAA-Record for '" . $domain . "' with ip '" . $ipv6 . "' successfully.");
         }
     } else {
@@ -100,29 +106,33 @@ foreach ($domains as $domain) {
             if ($ipv4 == $record["content"]) {
                 wlog("INFO", "Skipped record, because ipv4 is already up-to-date.");
                 continue;
-            } else if ($record["type"] == "A") {
-                $response = cf_curl("zones/" . $zone . "/dns_records/" . $record["id"], array("type" => "A", "name" => $domain, "content" => $ipv4, "ttl" => 120, "proxied" => $proxy), true);
-                if (!$response["success"])
+            } else if ($ipv4 && $record["type"] == "A") {
+                $response = cf_curl("zones/" . $zone . "/dns_records/" . $record["id"], array("type" => "A", "name" => $domain, "content" => $ipv4, "ttl" => 1, "proxied" => $proxy), true);
+                if (!$response["success"]) {
                     wlog("ERROR", "Could not update record for '" . $domain . "'. Continue with next record, if available.");
-                else
+                    $result = "failure";
+                } else
                     wlog("INFO", "Updated A-Record with ip '" . $ipv4 . "' successfully.");
                 continue;
             }
             if ($ipv6 == $record["content"]) {
                 wlog("INFO", "Skipped record, because ipv6 is already up-to-date.");
                 continue;
-            } else if ($record["type"] == "AAAA") {
-                $response = cf_curl("zones/" . $zone . "/dns_records/" . $record["id"], array("type" => "AAAA", "name" => $domain, "content" => $ipv6, "ttl" => 120, "proxied" => $proxy), true);
-                if (!$response["success"])
+            } else if ($ipv6 && $record["type"] == "AAAA") {
+                $response = cf_curl("zones/" . $zone . "/dns_records/" . $record["id"], array("type" => "AAAA", "name" => $domain, "content" => $ipv6, "ttl" => 1, "proxied" => $proxy), true);
+                if (!$response["success"]) {
                     wlog("ERROR", "Could not update record for '" . $domain . "'. Continue with next record, if available.");
-                else
+                    $result = "failure";
+                } else
                     wlog("INFO", "Updated AAAA-Record with ip '" . $ipv6 . "' successfully.");
                 continue;
             }
         }
     }
 }
+echo ("Result: $result");
 wlog("INFO", "Script completed");
+exit();
 
 function cf_curl($url, $post = false, $put = false)
 {
@@ -152,3 +162,12 @@ function wlog($error, $msg)
     fwrite($log, date("Y-m-d H:i:s") . " - " . $error . " - " . $msg . "\n");
     fclose($log);
 }
+
+function exit_error($error_code, $msg)
+{
+    http_response_code($error_code);
+    echo ($msg);
+    exit();
+}
+?>
+
